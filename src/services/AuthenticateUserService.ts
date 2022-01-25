a023776c0c940b40bf7d38e2505e6144a7eee1e4
unknown <ygorsallesaniceto@gmail.com>
2022-01-25 19:45:57 -0300

    add: request api user github

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -8,11 +8,22 @@ import axios from "axios";
  * Retornar o token com as info do usuário logado
  */
 
+interface IAccessTokenResponse {
+  access_token: string
+}
+
+interface IUserResponse {
+  avatar_url: string,
+  login: string,
+  id: number,
+  name: string
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = 'https://github.com/login/oauth/access_token';
 
-    const response = await axios.post(url, null, {
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -20,6 +31,12 @@ class AuthenticateUserService {
       },
       headers: {
         'Accept': "application/json"
+      }
+    });
+
+    const response = await axios.get('https://api.github.com/user', {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`,
       }
     });
 
