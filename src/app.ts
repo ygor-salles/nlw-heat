a5c1b8070241ceccbec79d45b8ada70f640cd24e
unknown <ygorsallesaniceto@gmail.com>
2022-01-29 17:11:47 -0300

    refactor: putting the initial routes in the routes file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import http from 'http';
 import cors from 'cors';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
 import { router } from './routes';
 
 const app = express();
@@ -20,14 +20,5 @@ io.on('connection', socket => console.log(`Usuário conectado no socket ${socket
 
 app.use(express.json());
 app.use(router);
-
-app.get('/github', (request: Request, response: Response) => {
-  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
-});
-
-app.get('signin/callback', (request: Request, response: Response) => {
-  const { code } = request.query;
-  return response.json(code);
-});
 
 export { serverHttp, io }
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { GetLast3MessageController } from "./controllers/GetLast3MessagesController";
@@ -6,6 +6,19 @@ import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middlware/ensureAuthenticated";
 
 const router = Router();
+
+router.get('/', (request: Request, response: Response) => {
+  response.json({ message: 'Welcome api-nlwheat' });
+})
+
+router.get('/github', (request: Request, response: Response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+
+router.get('signin/callback', (request: Request, response: Response) => {
+  const { code } = request.query;
+  return response.json(code);
+});
 
 router.post('/authenticate', new AuthenticateUserController().handle)
 
